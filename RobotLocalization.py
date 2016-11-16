f9f644e6f2d33c6f7089fb92bb77f30ef44cfb38
cunnia3 <andrew64ce@gmail.com>
2016-11-15 21:53:20 -0500

    Cleaned up the particle filter class, made particle scoring general

--- RobotLocalization.py
+++ RobotLocalization.py
@@ -6,12 +6,6 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.stats import norm
 
-# want a list of particle objects, want a function for 
-# want to be able to resample the list of particles using a transition model
-# eventually want to include observations for resampling -> WITHOUT MEASUREMENTS IT IS UNIFORM/NO RESAMPLING!!!
-
-
-
 def sample_normal_dist(b):
     """ Normal distribution specified by motion model slides """
     sum = 0
@@ -55,14 +49,15 @@ class ParticleFilter():
     def __init__(self, starting_particle, num_particles, starting_state_dist = 'None'):
         self.particle_list = []
         self.score_list = []
+        self.do_resample = False
         if starting_state_dist == 'None':  
             for i in range(num_particles):
                 self.particle_list.append(copy.deepcopy(starting_particle))
              
-    def update_filter(self, action, measurement = []):
+    def update_filter(self, action,scoring_func=[], measurement=[]):
         """ Update the particle filter with an action and a measurement"""
         self._update_particles(action)
-        self._measurement_score(measurement)
+        self._measurement_score(scoring_func, measurement)
         
         if self.do_resample:
             self._resample()
@@ -73,39 +68,32 @@ class ParticleFilter():
         for particle in self.particle_list:
             particle.transition(action)
             
-    def _measurement_score(self, measurement = 100):
+    def _measurement_score(self, scoring_func=[], measurement=[]):
         """  Use external measurements to assign scores to particles
-        @measurement: the measured distance to circle will be [] if outside of circle"""
-        circleX = 0
-        circleY = 0
-        circleR = .5
-        self.score_list = []
-        self.do_resample =False
-        i = 0
-        for particle in self.particle_list:
-            i+=1
-            particle_dist = math.sqrt((circleX - particle.x)**2 + (circleY - particle.y)**2)
-            # IF WE MEASURE OUTSIDE, BUT PARTICLE INSIDE
-            if measurement == [] and particle_dist < circleR:
-                self.do_resample= True
-                self.score_list.append(0)
+        @scoring_func: the function that determines the fitness of a particle
+                        SPECIAL VALUES -100: bad particle RESAMPLE THIS ONE"""
+
+        self.score_list = [1.0/(1.0*len(self.particle_list))]*len(self.particle_list)
+        
+        if scoring_func == []:
+            self.do_resample = False
+
+        # If there is a scoring function, use it
+        else:
+            self.do_resample = False
+            for i in range(len(self.particle_list)):
+                score = scoring_func(self.particle_list[i],measurement)
+                if score >= 0: # only resample if we get additional info
+                    self.do_resample = True
+                    self.score_list[i]=score
                 
-            # IF BOTH PARTICLE AND MEASUREMENT OUTSIDE
-            elif measurement > circleR:
-                self.score_list.append(circleR)
-                
-            # BOTH PARTICLE AND MEASURE INSIDE
-            else:
-                self.do_resample= True
-                self.score_list.append(norm.pdf(measurement-particle_dist,scale=.1))
-                
+        # Normalize
         self.score_list = np.asarray(self.score_list)
         self.score_list = self.score_list/np.sum(self.score_list)
         return
         
     def _resample(self):
         new_particles_list = []
-        
         
         for i in range(len(self.particle_list)):
             rand = np.random.rand()
@@ -120,6 +108,25 @@ class ParticleFilter():
         self.particle_list = new_particles_list
         return
        
+class Sensor():
+    def __init__(self):
+        return
+        
+class RangeSensor(Sensor):
+    def __init__(self, x, y, circleR):
+        self.circleX = x
+        self.circleY = y
+        self.circleR = circleR
+        
+    def score_particle(self, particle, measurement):
+        """@particle: robot pose particle
+           @measurement: true robot distance"""
+        particle_dist = math.sqrt((self.circleX - particle.x)**2 + (self.circleY - particle.y)**2)
+        if measurement < self.circleR:
+            return norm.pdf(measurement - particle_dist, scale=.05)
+        else:
+             return -1 # we have no info
+         
 def init():
     global particles, my_particle_filter, real_robotp
     particles.set_data([], [])
@@ -128,19 +135,16 @@ def init():
     
 ## TODO: Put this function into particle filter class and potentially seperate it with data
 def animate(i):
-    global fig, my_particle_filter, real_robot, real_robotp
-    circleX = 0
-    circleY = 0
-    circleR = .5
-    action = [0.01, 0,.01]
+    global fig, my_particle_filter, real_robot, real_robotp, range_sensor
+
+    action = [0.01, 0, .01]
     real_robot.transition(action)
     real_robotp.set_data([real_robot.x], [real_robot.y])
-    measurement = math.sqrt((circleX - real_robot.x)**2 + (circleY - real_robot.y)**2)
-    if measurement > circleR:
-        measurement = []
-    my_particle_filter.update_filter(action, measurement)
-    print measurement
     
+    measurement = math.sqrt((range_sensor.circleX - real_robot.x)**2 + (range_sensor.circleY - real_robot.y)**2)
+    my_particle_filter.update_filter(action, range_sensor.score_particle, measurement)
+    
+    print len(my_particle_filter.particle_list)
     ## MAKE INTO PLOTTABLE NUMPY
     particle_array = np.zeros([2,200])
     i=0
@@ -158,7 +162,8 @@ my_particle = OdometryParticle()
 my_particle_filter = ParticleFilter(my_particle, 200)
 
 real_robot = OdometryParticle(0,0,0,[0,0,0,0])
-
+range_sensor = RangeSensor(0,0,.5)
+## ANIMATION
 fig = plt.figure()
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
@@ -170,20 +175,3 @@ particles, = ax.plot([], [], 'bo', ms=3)
 real_robotp, = ax.plot([], [], 'ro', ms=5)
 ani = animation.FuncAnimation(fig, animate, frames = 10, interval=10, blit=False, init_func = init)
 plt.show()
-
-
-circleX = 0
-circleY = 0
-circleR = .2
-action = [0.01, 0,.01]
-real_robot.transition(action) 
-measurement = math.sqrt((circleX - real_robot.x)**2 + (circleY - real_robot.y)**2)
-my_particle_filter.update_filter(action, measurement)
-
-## MAKE INTO PLOTTABLE NUMPY
-particle_array = np.zeros([2,100])
-i=0
-for particle in my_particle_filter.particle_list:
-    particle_array[0,i] = particle.x
-    particle_array[1,i] = particle.y
-    i+=1
